ee50c44557c584fb79ddb0ee0e32ecb1154cd499
Dan M. Larsen <larsendanmarius@gmail.com>
2024-08-05 15:07:47 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,60 +1,73 @@
-const mainEl = document.querySelector('main');
-const newsletterFormEl = document.querySelector('.newsletter__form');
-const emailEl = document.querySelector('.newsletter__email');
-const emailInputEl = document.querySelector('.newsletter__email-input');
-const emailValidationTextEl = document.querySelector('.newsletter__email-validation-text');
-const successMessageEl = document.querySelector('.success-message');
-const successMessageTextEl = successMessageEl.querySelector('.success-message__text');
+const mainEl = document.querySelector("main");
+const newsletterFormEl = document.querySelector(".newsletter__form");
+const emailEl = document.querySelector(".newsletter__email");
+const emailInputEl = document.querySelector(".newsletter__email-input");
+const emailValidationTextEl = document.querySelector(
+  ".newsletter__email-validation-text"
+);
+const successMessageEl = document.querySelector(".success-message");
+const successMessageTextEl = successMessageEl.querySelector(
+  ".success-message__text"
+);
+const dismissBtn = document.querySelector(".success-message__dismiss-btn");
 
 const emailRegExp = /^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/g;
+const invalidEmailText = `Valid email required`;
 
-let invalidForm = false;
+let submittedForm = false;
 
 const isValidEmail = function (email) {
-    const trimmedEmail = email;
-    return trimmedEmail.length > 0 && trimmedEmail.match(emailRegExp);
+  const trimmedEmail = email;
+  return trimmedEmail.length > 0 && trimmedEmail.match(emailRegExp);
 };
 
 const handleInvalidForm = function () {
-    emailValidationTextEl.textContent = 'Valid email required';
+  emailValidationTextEl.textContent = invalidEmailText;
 
-    emailEl.classList.add('invalid');
-
-    invalidForm = true;
+  emailEl.classList.add("invalid");
 };
 
 const handleValidForm = function (email) {
-    const successMessage = `A confirmation email has been sent to ${email}. Please open it and click the button inside to confirm your subscription.`;
-    successMessageTextEl.textContent = successMessage;
+  const successMessage = `A confirmation email has been sent to <strong>${email}</strong>. Please open it and click the button inside to confirm your subscription.`;
+  successMessageTextEl.innerHTML = successMessage;
 
-    newsletterFormEl.classList.add('hidden');
-    successMessageEl.classList.remove('hidden');
+  newsletterFormEl.classList.add("hidden");
+  successMessageEl.classList.remove("hidden");
 };
 
 const handleSubmit = function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const emailInput = emailInputEl.value.trim();
+  submittedForm = true;
+  const emailInput = emailInputEl.value.trim();
 
-    if (!isValidEmail(emailInput)) {
-        handleInvalidForm();
-    } else {
-        handleValidForm(emailInput);
-    }
+  if (!isValidEmail(emailInput)) {
+    handleInvalidForm();
+  } else {
+    handleValidForm(emailInput);
+  }
 };
 
-newsletterFormEl.addEventListener('submit', handleSubmit);
+const handleInput = function (e) {
+  if (!submittedForm) return;
 
-emailInputEl.addEventListener('input', e => {
-    if (!invalidForm) return;
+  const emailInput = emailInputEl.value.trim();
 
-    const emailInput = emailInputEl.value.trim();
+  if (isValidEmail(emailInput)) {
+    emailValidationTextEl.textContent = "";
+    emailEl.classList.remove("invalid");
+    emailEl.classList.add("valid");
+  } else {
+    emailValidationTextEl.textContent = invalidEmailText;
+    emailEl.classList.remove("valid");
+    emailEl.classList.add("invalid");
+  }
+};
 
-    if (isValidEmail(emailInput)) {
-        emailValidationTextEl.textContent = '';
-        emailEl.classList.remove('invalid');
-        emailEl.classList.add('valid');
-    }
+const handleDismiss = function () {
+  location.reload();
+};
 
-    console.log(emailInputEl.value);
-});
+newsletterFormEl.addEventListener("submit", handleSubmit);
+emailInputEl.addEventListener("input", handleInput);
+dismissBtn.addEventListener("click", handleDismiss);
